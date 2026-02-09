66d84c1c2c3eed258faa454b48523bc7e9b29fe0
katzmeowtside <katzmeowtside@gmail.com>
2026-02-09 01:02:07 -0600

    Fix renderer path in albumCoverGenerator

--- modules/evidence/albumCoverGenerator.js
+++ modules/evidence/albumCoverGenerator.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const renderer = require('../graphics/renderer.js');
+const renderer = require('../../graphics/renderer.js');
 
 class AlbumCoverGenerator {
     constructor() {
